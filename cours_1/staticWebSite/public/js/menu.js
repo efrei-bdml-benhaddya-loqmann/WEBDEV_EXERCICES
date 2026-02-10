bebb0dc820051a8546f1c716e795c57e982aaecd
skezu <skez.test@gmail.com>
2026-02-10 15:13:18 +0100

    feat: completion tp 1.1 1.2 1.3 et 1.4

--- cours_1/staticWebSite/public/js/menu.js
+++ cours_1/staticWebSite/public/js/menu.js
@@ -1,9 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    const menuBtn = document.getElementById('menu-btn');
     const menuFlex = document.getElementById('menu-flex');
     const menu = document.getElementById('menu');
 
-    menuBtn.addEventListener('click', function() {
+    menuBtn.addEventListener('click', function () {
         menuFlex.classList.toggle('menu--open');
-        menu.classList.toggle('menu-col')
+        menu.classList.toggle('menu-col');
     });
 });
