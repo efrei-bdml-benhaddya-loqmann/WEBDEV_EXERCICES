0b5f0e99511283da7ab3b6526488cbc92f5113e2
skezu <skez.test@gmail.com>
2026-02-13 10:29:15 +0100

    feat: Completion TP3.1 3 bidirectionnel

--- cours_3/two-way-binding/src/App.tsx
+++ cours_3/two-way-binding/src/App.tsx
@@ -1,23 +1,35 @@
+import { useState } from 'react';
 import Review from './Review';
 
 function App() {
+  const [feedback, setFeedback] = useState('');
+  const [student, setStudent] = useState('');
+
+  const handleFeedbackChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setFeedback(event.target.value);
+  };
+
+  const handleStudentChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setStudent(event.target.value);
+  };
+
   return (
     <>
       <section id="feedback">
         <h2>Please share some feedback</h2>
         <p>
           <label>Your Feedback</label>
-          <textarea />
+          <textarea onChange={handleFeedbackChange} />
         </p>
         <p>
           <label>Your Name</label>
-          <input type="text" />
+          <input type="text" onChange={handleStudentChange} />
         </p>
       </section>
       <section id="draft">
         <h2>Your feedback</h2>
 
-        <Review />
+        <Review feedback={feedback} student={student} />
 
         <p>
           <button>Save</button>
