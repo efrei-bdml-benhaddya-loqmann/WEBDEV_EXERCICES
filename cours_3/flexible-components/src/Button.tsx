a8d328bdbbf0b8810dbb4caf33b967b1e1d7c807
skezu <skez.test@gmail.com>
2026-02-13 10:14:34 +0100

    feat: Completion TP3.1 2 button

--- cours_3/flexible-components/src/Button.tsx
+++ cours_3/flexible-components/src/Button.tsx
@@ -1,7 +1,19 @@
-export default function Button() {
- // Todo: Build this component!
- 
- // !!! Important: 
- // Wrap the icon with a <span className="button-icon"> to achieve the target look
- // Also wrap the children prop with a <span>
+
+
+interface ButtonProps {
+    mode?: 'filled' | 'outline' | 'text';
+    Icon?: React.ComponentType;
+    children: React.ReactNode;
+}
+
+export default function Button({ mode = "filled", Icon, children, ...props }: ButtonProps) {
+    return (
+        <button className={`${mode}-button button ${Icon ? "icon-button" : ""}`} {...props}>
+            {Icon && <span className="button-icon"><Icon /></span>}
+            <span>{children}</span>
+        </button>
+    )
+    // !!! Important: 
+    // Wrap the icon with a <span className="button-icon"> to achieve the target look
+    // Also wrap the children prop with a <span>
 }
