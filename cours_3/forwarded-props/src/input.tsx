4c77ffbd5b363984a09fd2ceadc9f957fc90f047
skezu <skez.test@gmail.com>
2026-02-13 10:03:48 +0100

    feat: Completion TP3.1 1 input

--- cours_3/forwarded-props/src/input.tsx
+++ cours_3/forwarded-props/src/input.tsx
@@ -1,8 +1,17 @@
+interface InputProps {
+    richText?: boolean;
+    type?: string;
+    placeholder: string;
+}
 
-const Input = () => {
+const Input = ({ richText, type, placeholder }: InputProps) => {
     // return a <textarea> if a richText prop is true
     // return an <input> otherwise
     // forward / set the received props on the returned elements
+    if (richText) {
+        return <textarea placeholder={placeholder} />
+    }
+    return <input type={type} placeholder={placeholder} />
 
 }
 
