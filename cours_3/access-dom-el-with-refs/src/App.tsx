b3f81ffc49e8214b2a95b0fc7d46df0e0aa9c40f
skezu <skez.test@gmail.com>
2026-02-13 11:56:15 +0100

    Completion TP3.2 1 image picker

--- cours_3/access-dom-el-with-refs/src/App.tsx
+++ cours_3/access-dom-el-with-refs/src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css'
-
+import { useRef } from 'react';
 function App() {
+  const fileInputRef = useRef<HTMLInputElement>(null);
   return (
     <div id="app">
       <p>Please select an image</p>
       <p>
-        <input data-testid="file-picker" type="file" accept="image/*" />
-        <button>Pick Image</button>
+        <input data-testid="file-picker" type="file" accept="image/*" ref={fileInputRef} />
+        <button onClick={() => fileInputRef?.current?.click()}>Pick Image</button>
       </p>
     </div>
   );
