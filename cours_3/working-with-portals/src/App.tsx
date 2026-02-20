0c842b808989bb4bf9602aeaa17f738c573b4590
skezu <skez.test@gmail.com>
2026-02-20 10:19:38 +0100

    feat: Completion TP3.2 5 Portals

--- cours_3/working-with-portals/src/App.tsx
+++ cours_3/working-with-portals/src/App.tsx
@@ -1,17 +1,23 @@
+import { useState } from 'react';
 import Toast from './Toast';
 
 function App() {
+  const [showToast, setShowToast] = useState(false);
+
   function handleEnrol() {
     // Todo: Show toast
+    setShowToast(true);
 
     setTimeout(() => {
       // Todo: hide toast
+      setShowToast(false);
     }, 3000);
   }
 
   return (
     <div id="app">
       {/* Todo: Render <Toast /> component (conditionally) here */}
+      {showToast && <Toast message="Enrol successful!" />}
       <article>
         <h2>React Course</h2>
         <p>
--- cours_3/working-with-portals/src/Toast.tsx
+++ cours_3/working-with-portals/src/Toast.tsx
@@ -1,7 +1,10 @@
-export default function Toast({ message }) {
-  return (
+import { createPortal } from "react-dom";
+
+export default function Toast({ message }: { message: string }) {
+  return createPortal(
     <aside className="toast" data-testid="toast">
       <p>{message}</p>
-    </aside>
+    </aside>,
+    document.querySelector('body')!
   );
 }
